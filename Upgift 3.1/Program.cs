c95044709e77383a059d49e64432823bd4775d3e
Admin <Admin@TEB123008>
2023-02-10 08:36:31 +0100

    Add project files.

---
+++ Upgift 3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hur gammal är du ?");
+        int ålder = Convert.ToInt32(Console.ReadLine());
+
+        if (ålder >= 16 && ålder <= 19)
+        {
+            Console.WriteLine("Du kan vara med tävlingen");
+        }
+        else
+        {
+            Console.WriteLine("Du kan tyvär inte vara med tävlingen");
+        }
+    }
+}
